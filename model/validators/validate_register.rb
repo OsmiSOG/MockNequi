b0f9d4a3c58f89f0574f67bc4931265132044473
Manuela García <dyg9812@gmail.com>
2018-12-01 20:45:37 -0500

    agregando documentacion de lo que hay por hacer

--- model/validators/validate_register.rb
+++ model/validators/validate_register.rb
@@ -4,32 +4,34 @@ require 'validate_register'
 class ValidateRegister
   attr_accesor :access
 
-  def initialize
-    @access = false
-  end
+    def initialize
+      @access = false
+    end
 
-  def validate_email(email)
-    if validates_format_of :email, :with => /@/
-      return true
-    else 
-      puts 'you have not entered a valid email address'
+    def validate_email(email)
+      if validates_format_of :email, :with => /@/
+        return true
+      else 
+        puts 'you have not entered a valid email address'
+    end
   end
-end
 
   private
-  def exist_account?(id)
-  account_database= return_element(q}@db_conection.query("SELECT 'id' FROM 'accounts' WHERE 'id'= '#{id}'")), 'id'
- if account_database != id
-  puts ' this account already exists, try again'
- else
-  puts 'valid account'
- end
-end
+    def exist_account?(id)
+    account_database= return_element(@db_conection.query("SELECT 'id' FROM 'accounts' WHERE 'id'= '#{id}'")), 'id'
+      if account_database != id
+        puts ' this account already exists, try again'
+      else
+        puts 'valid account'
+      end
+    end
 
 
-  def return_element(element, name)
-    element.each do |i|
-      return i[name]
+    def return_element(element, name)
+      element.each do |i|
+        return i[name]
+      end
     end
   end
 end
+
