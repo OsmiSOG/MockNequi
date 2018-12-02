a9973e6cd11a3ef65d58fdce09015737985d13e0
Manuela García <dyg9812@gmail.com>
2018-12-01 19:16:44 -0500

    agregando metodo para verificar si el email existe en la base de datos

--- model/validators/validate_login.rb
+++ model/validators/validate_login.rb
@@ -28,9 +28,17 @@ class ValidateLogin
   
   end
 
+  def validate_exits_email(email)
+    email_database= return_element(@db_conection.query("SELECT 'email' FROM 'users' WHERE 'id'= '#{id}'")), 'email')
+    if email_database != email
+      return false
+    else
+      @existing_user= user.new(db_conection, id)
+  end
+
   def validate_password(password)
     password = Digest::SHA2.hexdigest(password)
-    password_database = return_element(@db_conection.query("SELECT `password` FROM `users` WHERE `id` = '#{id}'"), 'password')
+    password_database = return_element(@db_conection.query("SELECT 'password' FROM 'users' WHERE 'id' = '#{id}'"), 'password')
 
     if password_database != password
       return false
